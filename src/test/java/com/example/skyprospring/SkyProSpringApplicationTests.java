f6c241c075c104b32f11f68077be8789bf9d7fdf
Arrowin24 <igor_rudenko.ru@mail.ru>
2022-12-08 18:48:22 +0700

    Commit

---
+++ src/test/java/com/example/skyprospring/SkyProSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
